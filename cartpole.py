dfd7205e0aaec15cd952c1fe798643113a36d3e4
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2025-01-03 14:54:57 +0100

    Update cartpole.py

--- cartpole.py
+++ cartpole.py
@@ -5,16 +5,18 @@ import random
 # Create env
 env = gym.make("CartPole-v1", render_mode="human")
 
-episods = 10
-for ep in range(1, episods+1):
-    state = env.reset()
+episodes = 10
+for ep in range(1, episodes+1):
+    state, _ = env.reset()
     done = False
     score = 0
 
     while not done:
         action = random.choice([0, 1])
-        _, reward, done =  env.step(action)
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
         score += reward
+        state = next_state
         env.render()
 
     print(f"Episode {ep}, Score: {score}")
